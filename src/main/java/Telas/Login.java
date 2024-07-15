01c5e8c78822c1863ac304411acebc26c9c570b0
Railan Santos <railanssousa.santos@gmail.com>
2024-07-15 00:29:05 -0300

    Nada

--- src/main/java/Telas/Login.java
+++ src/main/java/Telas/Login.java
@@ -14,7 +14,7 @@ public class Login extends javax.swing.JFrame {
         conexao = Conexao.conector();
         
         if (conexao !=null){
-            lblStatus.setText("Conectadoaa");
+            lblStatus.setText("Conectado");
         }else{
             lblStatus.setText("Não conectado");
         }
